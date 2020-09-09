460ad885b515f29c30688003aef25753bbfb64cc
qodirovshohijahon <tasaddux@gmail.com>
2020-09-09 12:25:05 +0500

    feat: header implemented

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   Grid
 } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
-import { makeStyles, ThemeProvider } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,7 +23,29 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1
-  }
+  },
+  mainFeaturesPost: {
+    position: "relative",
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPostion: "center"
+  },
+overlay: {
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  right: 0,
+  left: 0,
+  backgroundOverlay: "rgba(0,0,0, 0.3)"
+},
+mainFeaturesPostContent: {
+  position: "relative",
+  padding: theme.spacing(6),
+  marginTop: theme.spacing(8)
+}
 }))
 
 function App() {
@@ -54,27 +76,27 @@ function App() {
     </AppBar> 
 
     <main>
-      <Paper className={classes.mainFeaturesPost} styles={{backgroundImage: `url(https://source/unsplash.com/random)`}}>
+      <Paper className={classes.mainFeaturesPost} style={{ backgroundImage: `url(https://source.unsplash.com/random)`}}>
         <Container fixed>
+          <div className={classes.overlay} />
           <Grid container>
             <Grid item md={6}>
               <div className={classes.mainFeaturesPostContent}>
                 <Typography
                   component="h1"
+                  variant="h3"
                   color="inherit"
                   gutterBottom
                 >
                   Web developer blog
                 </Typography>
                 <Typography
-                  component="h5"
+                  variant="h5"
                   color="inherit"
                   paragraph
                 >
-                  Lorem Lorem dasdasdasdsfjdsfdsf
-                  Lorem Lorem dasdasdasdsfjdsfdsf
-                  Lorem Lorem dasdasdasdsfjdsfdsf
-                  Lorem Lorem dasdasdasdsfjdsfdsf
+                  Lorem Lorem dasdasdasdsfjdsfdsf Lorem Lorem dasdasdasdsfjdsfdsf
+                  Lorem Lorem dasdasdasdsfjdsfdsf Lorem Lorem dasdasdasdsfjdsfdsf
                   Lorem Lorem dasdasdasdsfjdsfdsf
                 </Typography>
                 <Button variant="contained" color="secondary">
@@ -85,6 +107,34 @@ function App() {
           </Grid>  
         </Container>    
       </Paper>
+      <div className={classes.mainContent}>
+        <Container maxWidth="md">
+          <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+            Web Developer Blog
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+          Lorem Lorem dasdasdasdsfjdsfdsf Lorem Lorem dasdasdasdsfjdsfdsf
+                  Lorem Lorem dasdasdasdsfjdsfdsf Lorem Lorem dasdasdasdsfjdsfdsf
+                  Lorem Lorem dasdasdasdsfjdsfdsf
+          </Typography>
+          <div className={classes.mainButtons}>
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Button variant="contained" color="primary">
+                  Start Now
+                </Button>
+
+              </Grid>
+              <Grid item>
+                <Button variant="outlined" color="primary">
+                  Learn More
+                </Button>
+                
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
     </main>
     </>
   );
